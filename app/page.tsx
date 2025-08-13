bc5dc1d4623ad2422ef47375789c7f07cfc26d8a
boatfieldflyfishing-debug <boatfieldflyfishing@gmail.com>
2025-08-13 10:34:31 +0100

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Wildish Club</h1>
-      <p>If you can see this, the app is routing correctly 🎉</p>
-      <p>Try <a href="/auth">/auth</a> or <a href="/places">/places</a> next.</p>
-    </main>
-  );
-}
