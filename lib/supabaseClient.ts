88dd6375146e314752e420990398aee230fb4241
boatfieldflyfishing-debug <boatfieldflyfishing@gmail.com>
2025-08-13 11:44:44 +0100

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,9 @@
 'use client';
 import { createClient } from '@supabase/supabase-js';
+
 export function supabaseClient() {
-  return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!);
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,          // keep
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!      // <-- change to NEXT_PUBLIC_
+  );
 }
